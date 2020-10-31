62968b736dfb491eee170e20c36f2b8abad8c779
Shubham Singh <sss.1771997@gmail.com>
2020-10-31 13:38:26 +0530

    fetch data in about page from py server

--- react-client/src/Pages/About.js
+++ react-client/src/Pages/About.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 
 export default function About() {
+    const [data, setData]= useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api`);
+            const body = await result.json();
+            console.log(body);
+            setData(body);
+        };
+        fetchData();
+    }, []);//empty for effect when load and then name changes
+    
     return (
         <div>
-            <h1>About Page</h1>
+            <ul>
+                {data.map((el,key)=> <li key={key}>{el.email}</li> )}
+            </ul>
+           
         </div>
     )
 }
