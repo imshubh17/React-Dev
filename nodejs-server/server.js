ada35fdf7691d4948810681c5e2c3ae635071029
Shubham Singh <sss.1771997@gmail.com>
2020-11-01 20:31:43 +0530

    api/signup routing

--- nodejs-server/server.js
+++ nodejs-server/server.js
@@ -16,7 +16,25 @@ fs.readFile('data.json', (err, data) => {
 
 //Routing
 app.get('/',(req, res) => res.status(200).send("Server page !!"));
-app.get('/api',(req, res) => res.status(200).send(JSON.stringify(rawData)));  
+
+app.get('/api',(req, res) => res.status(200).send(JSON.stringify(rawData)));
+
+app.post('/api/user/signup',(req,res)=>{    
+    withDB(async (db)=> {        
+        const {FirstName, LastName, email, password} = req.body;
+        const userData = {FirstName, LastName, email, password};
+        //const userInfo = await db.collection('Users').findOne({ email: email })
+        await db.collection('Users').insertOne(userData, async (err, res)=>{
+            if (err){
+                res.status(401).json({message:"Registration failed", error: err});
+            }
+            else{
+                const registerdUser = await db.collection('Users').findOne({ email: email });
+                res.status(200).json(updatedArticleInfo); 
+            }
+        });      
+    }, 'ReactDev', res);       
+});
 
 //server listening
 app.listen(8000, ()=>console.log('server listnning at localhost:8000'));
