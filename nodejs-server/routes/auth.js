9ef26f6c40595871d6a110ee6a14093d86cbaa19
Shubham Singh <sss.1771997@gmail.com>
2020-12-06 13:28:56 +0530

    login authentication

--- nodejs-server/routes/auth.js
+++ nodejs-server/routes/auth.js
@@ -1,7 +1,8 @@
 import express from "express";
 import User from "../model/User";
 const router = express.Router();
-import {registerValidation} from "../validation";
+import {registerValidation, loginValidation} from "../validation";
+import bcrypt from "bcryptjs";
 
 router.post('/register',async (req, res)=> {
 
@@ -9,11 +10,19 @@ router.post('/register',async (req, res)=> {
     const {error} = registerValidation(req.body)
     if(error) return res.status(400).send(error.details[0].message);
 
+    //checking if user already exists
+    const emailExist = await User.findOne({email:req.body.email});
+    if (emailExist) return res.status(400).send('Email already exists');
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
     const user = new User({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
 
     try {
@@ -26,8 +35,23 @@ router.post('/register',async (req, res)=> {
     
 })
 
-router.post('/login', (req, res)=> {
-    res.send('Login');
+//Login
+router.post('/login',async (req, res)=> {
+
+    //validate data
+    const {error} = loginValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message);
+
+     //checking if email exists
+     const user  = await User.findOne({email:req.body.email});
+     if (!user) return res.status(400).send('Email doesn\'t exists');
+
+     //password is correct
+     const validPass = await bcrypt.compare(req.body.password, user.password);
+     if (!validPass) return res.status(400).send('Password is incorrect');
+
+
+    res.send('Login success');
 })
 
 module.exports = router;
